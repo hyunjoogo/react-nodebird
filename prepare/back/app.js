cf387a080d129442578655b5c15b6147be41b1e7
HyunJoo Kim <hyunjoogo@gmail.com>
2022-02-04 23:30:57 +0900

    express 설치하기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,9 +1,18 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.end('hello node');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello api');
 });
-server.listen(3065, () => {
+app.get('/home', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello1' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.listen(3065, () => {
   console.log('서버 실행 중');
 });
